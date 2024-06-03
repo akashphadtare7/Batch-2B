1b9289db3c1e0fba14cef9229445199866315eba
akashphadtare7 <akashphadtare7@gmail.com>
2024-06-03 08:03:05 +0530

    added code for multiplication

--- Arithmatic.py
+++ Arithmatic.py
@@ -2,7 +2,14 @@ def add(num1, num2):
     ans = num1 + num2
     return ans
 
-a = 18
+def mul(num1, num2):
+    ans = num1 * num2
+    return ans
+
+a = 10
 b = 20
 ans = add(a,b)
 print(f"addition of {a} and {b} is {ans}")
+
+ans = mul(a,b)
+print(f"multiplication of {a} and {b} is {ans}")
