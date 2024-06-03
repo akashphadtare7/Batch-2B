341422a0f0b81cb89764bd0e127f3045837f39f7
akashphadtare7 <akashphadtare7@gmail.com>
2024-06-03 08:13:59 +0530

    added code for division

--- Arithmatic.py
+++ Arithmatic.py
@@ -1,8 +1,14 @@
 def add(num1, num2):
     ans = num1 + num2
     return ans
+def div(num1, num2):
+    ans = num1 / num2
+    return ans
 
-a = 18
-b = 20
+a = 20
+b = 10
 ans = add(a,b)
 print(f"addition of {a} and {b} is {ans}")
+
+ans = div (a,b)
+print(f"division of {a} and {b} is {ans}")
